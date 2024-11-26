2ef0d3b8a88f0dbf6977b4885bc23fdd051b9bf0
sudoskys <coldlando@hotmail.com>
2024-11-26 15:34:29 +0800

    feat: refactor sidebar and admin layout components for improved structure and styling

--- frontend/components/app-sidebar.vue
+++ frontend/components/app-sidebar.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import {Home, User2, ChevronUp} from 'lucide-vue-next'
-import {
-  Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarGroupContent,
-  SidebarMenu, SidebarMenuItem, SidebarMenuButton, SidebarFooter
-} from '@/components/ui/sidebar'
 import {DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem} from '@/components/ui/dropdown-menu'
 import {useUserStore} from '~/stores/user'
 import {storeToRefs} from 'pinia'
 import {useRouter} from 'vue-router'
+import {
+  Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarGroupContent,
+  SidebarMenu, SidebarMenuItem, SidebarMenuButton, SidebarFooter
+} from '@/components/ui/sidebar'
 
 const userStore = useUserStore()
 const router = useRouter()
@@ -44,17 +44,21 @@ const items = [
 </script>
 
 <template>
-  <Sidebar>
+  <Sidebar
+      collapsible="icon"
+  >
     <SidebarContent>
       <SidebarGroup>
         <SidebarGroupLabel>应用面板</SidebarGroupLabel>
         <SidebarMenu>
           <SidebarMenuItem v-for="item in items" :key="item.title">
             <SidebarMenuButton as-child>
-              <NuxtLink :to="item.url" class="flex items-center gap-2">
-                <component :is="item.icon" class="h-4 w-4"/>
-                <span>{{ item.title }}</span>
-              </NuxtLink>
+              <Button variant="ghost">
+                <NuxtLink :to="item.url" class="flex items-center gap-2 w-full h-full">
+                  <component :is="item.icon" class="h-4 w-4"/>
+                  <span>{{ item.title }}</span>
+                </NuxtLink>
+              </Button>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
--- frontend/layouts/admin.vue
+++ frontend/layouts/admin.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-import { useAuthGuard } from '@/composables/useAuthGuard';
-import { useRoute } from 'vue-router';
-import { Toaster } from '@/components/ui/toast';
-import { SidebarProvider, SidebarInset,SidebarTrigger } from '@/components/ui/sidebar'
-import { Separator } from '@/components/ui/separator'
-import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
+import {useAuthGuard} from '@/composables/useAuthGuard';
+import {useRoute} from 'vue-router';
+import {Toaster} from '@/components/ui/toast';
+import {SidebarProvider, SidebarInset, SidebarTrigger} from '@/components/ui/sidebar'
+import {Separator} from '@/components/ui/separator'
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from '@/components/ui/breadcrumb'
 
 const route = useRoute();
 
@@ -19,36 +26,37 @@ useAuthGuard({
 <template>
   <div>
     <SidebarProvider
-      :style="{
+        :style="{
         '--sidebar-width': '16rem',
         '--sidebar-width-mobile': '16rem'
       }"
     >
-      <AppSidebar />
+      <AppSidebar/>
       <SidebarInset>
-        <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header
+            class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div class="flex items-center gap-2 px-4">
-            <SidebarTrigger class="-ml-1" />
-            <Separator orientation="vertical" class="mr-2 h-4" />
+            <SidebarTrigger class="-ml-1"/>
+            <Separator orientation="vertical" class="mr-2 h-4"/>
             <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem class="hidden md:block">
-                <BreadcrumbLink href="#">
-                  仪表盘
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator class="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>{{ route.name }}</BreadcrumbPage>
-              </BreadcrumbItem>
+              <BreadcrumbList>
+                <BreadcrumbItem class="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    仪表盘
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator class="hidden md:block"/>
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{{ route.name }}</BreadcrumbPage>
+                </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
           </div>
         </header>
         <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <slot />
+          <slot/>
         </div>
-        <Toaster />
+        <Toaster/>
       </SidebarInset>
     </SidebarProvider>
   </div>
